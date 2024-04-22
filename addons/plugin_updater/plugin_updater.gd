e6b75c5b6e12a8803eaa2ccf31afbf8a1cf817e3
Myyk Seok <2080820+myyk@users.noreply.github.com>
2024-04-22 17:07:50 +0800

    feat: improve the deleting old version code

--- addons/plugin_updater/plugin_updater.gd
+++ addons/plugin_updater/plugin_updater.gd
@@ -11,12 +11,16 @@ func _exit_tree():
 	pass
 
 func _install_to_plugin(plugin_name: String):
-	OS.move_to_trash(ProjectSettings.globalize_path("res://addons/%s/generated/updater" % plugin_name))
-
+	var err: Error = OK
 	# Copy addons/plugin-updater/core as addons/<plugin_name>/generated/updater
 	var source_path = "res://addons/plugin_updater/core/"
 	var target_path = "res://addons/%s/generated/updater/" % plugin_name
-	var err: Error = OK
+	
+	if DirAccess.dir_exists_absolute(target_path):
+		err = OS.move_to_trash(ProjectSettings.globalize_path(target_path))
+		if err != OK:
+			push_error("plugin_updater: could not remove previous install, error = " + str(err))
+
 	err = DirAccess.make_dir_recursive_absolute(target_path)
 	if err != OK:
 		push_error("plugin_updater: error making directory, error = " + str(err))
